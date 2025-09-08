f15a3b3961e69714777f3ba3bf0cdb2836abae6c
Hitury <xxhituryxx@gmail.com>
2025-09-08 11:18:57 +0200

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,18 @@ function App() {
         <div className="card card-border border-white bg-base-100 w-96 shadow-xl p-10">
           <div className="card-body">
             <h1 className="card-title">Todo App</h1>
-            <p>
-              Created by IJvadeli & Hitury
-            </p>
-            <List />
-            <div className="flex card-actions items-center justify-center mt-15">
-              <button className="btn btn-primary">Buy Now</button>
+            <p>Created by IJvadeli & Hitury</p>
+            <div className="flex card-actions flex-row items-center justify-center mt-5 mb-5">
+              <div className="join">
+                <div>
+                  <label className="input validator join-item">
+                    <input type="text" placeholder="Enter a Todo" required />
+                  </label>
+                </div>
+                <button className="btn btn-neutral join-item">Join</button>
+              </div>
             </div>
+            <List />
           </div>
         </div>
       </div>
