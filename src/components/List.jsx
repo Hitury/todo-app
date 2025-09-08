3fd72ea0157ccdefa2b5bcb4ce5da959770b8f83
Hitury <xxhituryxx@gmail.com>
2025-09-08 11:01:43 +0200

    dev

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -8,7 +8,7 @@ function List() {
     <div><img className="size-10 rounded-box" src="https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/large/800/Dio-Brando.Jojo-s-Bizarre-Adventure.webp"/></div>
     <div>
       <div>Dio</div>
-      <div className="text-xs uppercase font-semibold opacity-60">NIGGER</div>
+      <div className="text-xs uppercase font-semibold opacity-60">MIJN GOAT</div>
     </div>
     <button className="btn btn-square btn-ghost">
       <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
